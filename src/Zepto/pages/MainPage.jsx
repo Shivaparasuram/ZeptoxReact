8028ea8037c4dbe9ec8dfd30893e091e596d781f
Shiva Parasuram R <shivaparasuramr@gmail.com>
2026-02-12 11:31:47 +0530

    commit

---
+++ src/Zepto/pages/MainPage.jsx
@@ -0,0 +1,16 @@
+import Header from '../components/Header.jsx';
+import Banner from '../components/Banner.jsx';
+import Body from '../components/Body.jsx';
+import Footer from '../components/Footer.jsx';    // .. to get inside the file
+
+const MainPage=()=>{
+    return (
+        <div>
+            <Header/>
+            <Banner/>
+            <Body/>
+            <Footer/>
+        </div>
+       )
+    }
+export default MainPage;
